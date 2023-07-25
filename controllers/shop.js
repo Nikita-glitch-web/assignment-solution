1c9aeff887c3083ba5f17b993815e333784ad9bd
Nick <nikitalytvynov0506@gmail.com>
2023-07-25 17:21:37 +0300

    top

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,7 +9,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
-        isAuthenticated: req.session.isLoggedIn
+      
       });
     })
     .catch(err => {
@@ -25,7 +25,7 @@ exports.getProduct = (req, res, next) => {
         product: product,
         pageTitle: product.title,
         path: '/products',
-        isAuthenticated: req.session.isLoggedIn
+        
       });
     })
     .catch(err => console.log(err));
@@ -38,7 +38,6 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => {
@@ -47,7 +46,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.session.user
+  req.user
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
@@ -56,7 +55,7 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart',
         products: products,
-        isAuthenticated: req.session.isLoggedIn
+        
       });
     })
     .catch(err => console.log(err));
@@ -94,8 +93,8 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.session.user.name,
-          userId: req.session.user
+          email: req.user.email,
+          userId: req.user
         },
         products: products
       });
@@ -117,7 +116,6 @@ exports.getOrders = (req, res, next) => {
         path: '/orders',
         pageTitle: 'Your Orders',
         orders: orders,
-        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,18 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
+router.get("/signup", authController.getSignup);
+
 router.post('/login', authController.postLogin);
+
+router.post("/signup", authController.postSignup);
+
+router.post('/logout', authController.postLogout)
+
+router.get('/reset', authController.getReset);
+
+router.get('/reset', authController.postReset)
+
+router.get('/reset/:token', authController.getNewPassword);
 
 module.exports = router;
